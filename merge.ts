88cc006934cf924e408eb561b748d6bfacf5e3d6
binbutsa2140 <bam.356345@gmail.com>
2026-06-14 12:16:47 +0700

    finished mian code

--- merge.ts
+++ merge.ts
@@ -4,23 +4,50 @@ let c3 = [11, 12, 13, 14, 15];
 
 const merge = (c1: number[], c2: number[], c3: number[]):number[] => {
 
-    let c1Start = 0
-    let c2Start = c2.length-1
-    let c3Start = 0
 
+    let c1Runner:number= 0
+    let c2Runner:number = c2.length-1
+    let c3Runner:number = 0
+    let result:number[] = []
     
+    while (c1Runner<c1.length || c2Runner>=0 || c3Runner<c3.length){
 
-    while (c1Start<c1.length || c2Start>c2.length || c3Start<c3.length){
+
+        let val1 = c1[c1Runner] || Infinity //ยังไงก็หาค่าน้อยสุด เอาไว้กันค่า undefined
+        let val2 = c2[c2Runner] || Infinity
+        let val3 = c3[c3Runner] || Infinity
+
+
+        let min:number = val1
+        if(val2<min){
+            min = val2
+        }
+        if(val3<min){
+            min = val3
+        }
+        result.push(min)
+
+        if(min==val1){
+            c1Runner+=1
+        }
+        else if(min==val2){
+            c2Runner-=1
+        }
+        else if(min==val3)[
+            c3Runner+=1
+        ]
+
         
+
     }
     
 
-    let result:number[] = []
     
-
-    for(int io=0;i<)
+    
 
     return result
 };
 
+
+console.log(merge(c1, c2, c3))
 
