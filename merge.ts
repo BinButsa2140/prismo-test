30dab048f34786ac3e2a8e223d8ce98d4afac5f1
binbutsa2140 <bam.356345@gmail.com>
2026-06-14 11:22:40 +0700

    initial git

---
+++ merge.ts
@@ -0,0 +1,26 @@
+let c1 = [1, 2, 3, 4, 5];
+let c2 = [10, 9, 8, 7, 6];
+let c3 = [11, 12, 13, 14, 15];
+
+const merge = (c1: number[], c2: number[], c3: number[]):number[] => {
+
+    let c1Start = 0
+    let c2Start = c2.length-1
+    let c3Start = 0
+
+    
+
+    while (c1Start<c1.length || c2Start>c2.length || c3Start<c3.length){
+        
+    }
+    
+
+    let result:number[] = []
+    
+
+    for(int io=0;i<)
+
+    return result
+};
+
+
